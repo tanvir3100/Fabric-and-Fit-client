ef84b7a2523bbf2595c48a944d9e3d9b4424267d
tanvir <tanvir0046@gmail.com>
2024-01-12 19:03:35 +0600

    some modify is done

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
                 {/* <div className="hero-overlay bg-opacity-40"></div> */}
                 <div className="flex items-start justify-start w-4/5">
                     <div className="w-full">
-                        <h1 className="mb-5 text-5xl font-bold text-white">Hello there</h1>
-                        <h1 className="mb-5 text-4xl text-white font-bold">We Are Fabric & Fit</h1>
-                        <h1 className='text-amber-300 mt-5 text-4xl font-bold'>
+                        <h1 className="mb-5 text-3xl lg:text-5xl font-bold text-white">Hello there</h1>
+                        <h1 className="mb-5 text-3xl lg:text-4xl text-white font-bold">We Are Fabric & Fit</h1>
+                        <h1 className='text-amber-300 mt-5 text-3xl lg:text-4xl font-bold'>
                             <Typewriter words={['Where Fashion', 'Meets Comfort in', 'Every Thread!']}
                                 loop={5}
                                 cursor
